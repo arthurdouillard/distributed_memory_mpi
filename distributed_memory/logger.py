4503176dfbffd224b6964bd106ed382867f8708b
Arthur Douillard <douill_a@epita.fr>
2017-12-05 17:42:28 +0100

    Fix logger that did not return value.

--- distributed_memory/logger.py
+++ distributed_memory/logger.py
@@ -5,6 +5,6 @@ def log(msg):
         def wrap(self, *args, **kwargs):
             self.log('Clock: {}\t'.format(self.clock.timer) +\
                     msg + ': {} {}'.format(args, kwargs))
-            f(self, *args, *kwargs)
+            return f(self, *args, *kwargs)
         return wrap
     return wrapper
