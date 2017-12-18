0f31a354730d332a368db2e3b4c69f9825c223c2
Arthur Douillard <douill_a@epita.fr>
2017-12-18 14:51:59 +0100

    Update demo with options.

--- demo.py
+++ demo.py
@@ -10,17 +10,16 @@ import distributed_memory as dm
 
 
 def parse_args(argv):
-    """Parse arguments and call the Master.
-
-    For help, call:
-        $ python -m distributed_memory -h
-    """
-    parser = argparse.ArgumentParser(description='Demo of the distributed memory')
-    parser.add_argument('--mem', action='store', type=int, dest='mem',
-                        default=10, help='Max size in elements for each processus.')
+    """Parse arguments and call the Master."""
+    parser = argparse.ArgumentParser(description='Demo of the distributed memory:')
+    parser.add_argument('--size', action='store', type=int, dest='size',
+                        help='Size of the list stored on the distributed memory.',
+                        default=20)
+    parser.add_argument('--verbose', action='store_true', dest='verbose',
+                        help='Verbose mode.')
     parser.add_argument('--log', action='store', type=str, dest='log',
                         choices=['critical', 'debug', 'info'],
-                        default='critical', help='Log level')
+                        default='critical', help='Log level.')
 
     args = parser.parse_args(argv)
 
@@ -31,22 +30,29 @@ def parse_args(argv):
     elif args.log == 'info':
         logging.basicConfig(level=logging.INFO)
 
-    mem = dm.init_memory(max_per_slave=args.mem)
-    demo(mem)
+    nb_slaves = MPI.COMM_WORLD.Get_size() - 1
+    max_per_slave = args.size / nb_slaves
+    mem = dm.init_memory(max_per_slave=max_per_slave)
+
+    demo(mem, args.size, args.verbose)
 
 
-def demo(mem):
-    var = mem.add(list(range(10)))
-    print('Initial value:', mem.read(var))
+def demo(mem, size, verbose):
+    var = mem.add(list(range(size)))
+    if verbose:
+        print('Initial value:', mem.read(var))
 
     mem.map(var, lambda x: x ** 2)
-    print('After squaring:', mem.read(var))
+    if verbose:
+        print('After squaring:', mem.read(var))
 
     mem.filter(var, lambda x: x % 2 != 0)
-    print('After removing even integer:', mem.read(var))
+    if verbose:
+        print('After removing even integer:', mem.read(var))
 
-    val = mem.reduce(var, lambda x, y: x * y, 1)
-    print('Multiplication of the remaining elements:', val)
+    val = mem.reduce(var, lambda x, y: x * y, 0)
+    if verbose:
+        print('Multiplication of the remaining elements:', val)
 
     mem.quit()
 
